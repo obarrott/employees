d196752d247746237837d4f617bba6400751300d
obarrott <71147609+obarrott@users.noreply.github.com>
2021-04-07 11:52:48 -0600

    added gitignore

--- client/src/components/EmployeeList.jsx
+++ client/src/components/EmployeeList.jsx
@@ -1,21 +1,17 @@
 import React, {useEffect, useContext} from 'react';
 import EmployeeFinder from '../apis/EmployeeFinder';
-//import RandomUser from '../apis/RandomUser';
 import { EmployeesContext } from '../contexts/EmployeesContext';
 import { useHistory } from 'react-router-dom';
-
-
 
 const EmployeeList = () => {
     const {employees, setEmployees} = useContext(EmployeesContext);
     let history = useHistory();
     useEffect(() => {
-            const fetchData = async () => {
-
+        const fetchData = async () => {
             try {
                 const response = await EmployeeFinder.get("/");
                 setEmployees(response.data.data.employees);
-                console.log(response); //This puts the end of the url on the url from RestaurantFinder.
+                console.log(response);
             } catch (error) {
                 console.log(error);
             };
@@ -29,32 +25,30 @@ const EmployeeList = () => {
 
     return (
         <div className="list-group">
-                <table className="table table-hover table-striped">
-                    <thead>
-                        <tr className=".bg-primary">
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">City</th>
-                            <th scope="col">Country</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {employees && employees.map((employee) => {
-                            return (
-                                <tr onClick={() => handleEmployeeSelect(employee.id)} key={employee.id}>
-                                    <td>{employee.first_name} {employee.last_name}</td>
-                                    <td>{employee.email}</td>
-                                    <td>{employee.city}</td>
-                                    <td>{employee.country}</td>  
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </div>
+            <table className="table table-hover table-striped">
+                <thead>
+                    <tr className=".bg-primary">
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees && employees.map((employee) => {
+                        return (
+                            <tr onClick={() => handleEmployeeSelect(employee.id)} key={employee.id}>
+                                <td>{employee.first_name} {employee.last_name}</td>
+                                <td>{employee.email}</td>
+                                <td>{employee.city}</td>
+                                <td>{employee.country}</td>  
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 };
-
-
 
 export default EmployeeList;
